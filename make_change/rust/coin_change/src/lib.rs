9ece947d788fb4ee2e2ad000dae735533ab9c5e6
ccdle12 <chris.coverdale24@gmail.com>
2019-03-15 09:23:36 +0000

    make_change/rust/coin_change: adding bottom up

--- make_change/rust/coin_change/src/lib.rs
+++ make_change/rust/coin_change/src/lib.rs
@@ -20,6 +20,12 @@ fn calc_change(target: i16, cache: &mut Vec<i16>, coins: &Vec<i16>) -> i16 {
 
     for coin in coins {
         if target - coin >= 0 {
+            println!(
+                "Calling recursively, target: {}, coin: {}, new target: {}",
+                target,
+                coin,
+                target - coin
+            );
             let current_min = calc_change(target - coin, cache, coins);
             if current_min < min_coins {
                 min_coins = current_min;
@@ -31,13 +37,41 @@ fn calc_change(target: i16, cache: &mut Vec<i16>, coins: &Vec<i16>) -> i16 {
     cache[target as usize]
 }
 
+#[allow(dead_code)]
+fn bottom_up(target: i16, cache: &mut Vec<i16>, coins: &Vec<i16>) -> i16 {
+    for i in 1..(target + 1) {
+        let mut min_coins = MAX;
+
+        for coin in coins {
+            println!("i-coins: i: {} coin: {}, result: {}", i, coin, i - coin);
+            if i - coin >= 0 {
+                let current_min = cache[(i - coin) as usize] + 1;
+                println!(
+                    "value at current_min index: {}, index: {}",
+                    current_min,
+                    i - coin
+                );
+                println!("current min: {}", current_min);
+                println!("min coins: {}", min_coins);
+                if current_min < min_coins {
+                    min_coins = current_min;
+                }
+            }
+            println!("index i: {}, assignin min_coins: {}", i, min_coins);
+            cache[i as usize] = min_coins;
+        }
+    }
+
+    println!("returned: {}", cache[target as usize]);
+    cache[target as usize]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn simple_input() {
-        // let change = Change::new(vec![10, 6, 1]);
         let change = min_change(1, vec![10, 6, 1]);
 
         assert_eq!(1, change);
@@ -46,6 +80,19 @@ mod tests {
     #[test]
     fn simple_input_2() {
         let change = min_change(17, vec![10, 6, 1]);
+        assert_eq!(3, change);
+    }
+
+    #[test]
+    fn test_bottom_up() {
+        let target = 17;
+
+        let mut cache = vec![0];
+        for _i in 1..target + 1 {
+            cache.push(-1);
+        }
+
+        let change = bottom_up(target, &mut cache, &vec![10, 6, 1]);
         assert_eq!(3, change);
     }
 }
