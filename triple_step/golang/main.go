2ad91cf7971c7edce36d4a450df8f355354f20dc
ccdle12 <chris.coverdale24@gmail.com>
2019-03-22 15:24:45 +0000

    triple_step/golang: adding iterative

--- triple_step/golang/main.go
+++ triple_step/golang/main.go
@@ -2,11 +2,9 @@ package main
 
 func main() {}
 
-// TripleStep is the open function for running a recursive function to find all
-// the possible combinations of "skipping steps" to reach the target step.
-func TripleStep(target int16) int16 {
-	steps := []int16{2, 3, 4}
+var steps = []int16{2, 3, 4}
 
+func genCache(target int16) []int16 {
 	// Initialise a cache with the length of target.
 	cache := []int16{}
 	for i := int16(0); i < target+1; i++ {
@@ -17,6 +15,16 @@ func TripleStep(target int16) int16 {
 
 		cache = append(cache, 0)
 	}
+
+	return cache
+}
+
+// TripleStep is the open function for running a recursive function to find all
+// the possible combinations of "skipping steps" to reach the target step.
+func TripleStep(target int16) int16 {
+
+	// Initialise a cache with the length of target.
+	cache := genCache(target)
 
 	return tripleStep(cache, steps, target)
 }
@@ -42,4 +50,31 @@ func tripleStep(cache []int16, steps []int16, target int16) int16 {
 	}
 
 	return cache[target]
+}
+
+func Iterative(target int16) int16 {
+	cache := genCache(target)
+
+	return iterative(cache, steps, target)
+}
+
+func iterative(cache []int16, steps []int16, target int16) int16 {
+	if target <= 2 {
+		return cache[target]
+	}
+
+	for i := int16(3); i < target+1; i++ {
+		for _, step := range steps {
+			newTarget := i - step
+
+			if newTarget < 0 {
+				continue
+			}
+
+			cache[i] = cache[i] + cache[newTarget]
+		}
+	}
+
+	return cache[target]
+
 }
--- triple_step/golang/main_test.go
+++ triple_step/golang/main_test.go
@@ -42,3 +42,32 @@ func TestRecursive(t *testing.T) {
 		}
 	}
 }
+
+func TestIterative(t *testing.T) {
+	// Tests (input, expected).
+	tests := []struct {
+		input    int16
+		expected int16
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 0},
+		{3, 1},
+		{4, 1},
+		{5, 2},
+		{6, 2},
+		{7, 4},
+		{8, 5},
+		{12, 24},
+	}
+
+	// Execute each test.
+	for _, test := range tests {
+		result := Iterative(test.input)
+
+		if result != test.expected {
+			t.Fatalf("result should have been %v, received: %v",
+				test.expected, result)
+		}
+	}
+}
