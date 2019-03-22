34c0344e0486c1936e7c8823aa830b4f06cc8b6f
ccdle12 <chris.coverdale24@gmail.com>
2019-03-22 15:11:59 +0000

    triple_step/golang: adding cache

--- triple_step/golang/main.go
+++ triple_step/golang/main.go
@@ -2,27 +2,44 @@ package main
 
 func main() {}
 
+// TripleStep is the open function for running a recursive function to find all
+// the possible combinations of "skipping steps" to reach the target step.
 func TripleStep(target int16) int16 {
 	steps := []int16{2, 3, 4}
 
-	return tripleStep(steps, target)
+	// Initialise a cache with the length of target.
+	cache := []int16{}
+	for i := int16(0); i < target+1; i++ {
+		if i == 1 {
+			cache = append(cache, 1)
+			continue
+		}
+
+		cache = append(cache, 0)
+	}
+
+	return tripleStep(cache, steps, target)
 }
 
-func tripleStep(steps []int16, target int16) int16 {
-	if target == 1 {
-		return 1
+func tripleStep(cache []int16, steps []int16, target int16) int16 {
+	if target < 0 {
+		return cache[0]
 	}
 
 	if target <= 2 {
-		return 0
+		return cache[target]
 	}
 
-	var result int16
+	if cache[target] != 0 {
+		return cache[target]
+	}
+
 	for _, step := range steps {
 		newTarget := target - step
-		current := tripleStep(steps, newTarget)
-		result = result + current
+
+		current := tripleStep(cache, steps, newTarget)
+		cache[target] = cache[target] + current
 	}
 
-	return result
+	return cache[target]
 }
