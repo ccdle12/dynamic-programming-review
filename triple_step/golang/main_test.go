eabc4b1bf72144acc0698027fa31e91256167cde
ccdle12 <chris.coverdale24@gmail.com>
2019-03-22 14:53:44 +0000

    triple_step/golang: updating tests

--- triple_step/golang/main_test.go
+++ triple_step/golang/main_test.go
@@ -14,12 +14,31 @@ func TestBaseCase(t *testing.T) {
 	}
 }
 
-func TestSimpleInput(t *testing.T) {
-	var input int16 = 3
-	var expected int16 = 1
+func TestRecursive(t *testing.T) {
+	// Tests (input, expected).
+	tests := []struct {
+		input    int16
+		expected int16
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 0},
+		{3, 1},
+		{4, 1},
+		{5, 2},
+		{6, 2},
+		{7, 4},
+		{8, 5},
+		{12, 24},
+	}
 
-	result := TripleStep(input)
-	if result != expected {
-		t.Fatalf("result should have been %v, received: %v", expected, result)
+	// Execute each test.
+	for _, test := range tests {
+		result := TripleStep(test.input)
+
+		if result != test.expected {
+			t.Fatalf("result should have been %v, received: %v",
+				test.expected, result)
+		}
 	}
 }
